e56ac121d51e99cd9c6e738ddc4f64468711640e
Ricco Arntz <riccoarntz@gmail.com>
2024-05-31 18:34:27 +0300

    test lint-staged.config.js

--- web/lint-staged.config.js
+++ web/lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // './src/**/*.{ts?(x),js?(x)}': () => ['npm run lint'],
+  './src/**/*.{ts?(x),js?(x)}': () => ['npm run lint'],
   './src/**/*.ts?(x)': () => ['npm run typecheck'],
   './src/**/*.{ts?(x),js,?(s)css,md,json}': () => ['npm run format'],
 };
