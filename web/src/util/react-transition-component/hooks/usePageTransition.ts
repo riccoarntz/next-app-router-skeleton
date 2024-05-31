bc35bcbc72ca20e4709acc7f1dffc2c1f0d47fcf
Ricco Arntz <riccoarntz@gmail.com>
2024-05-31 18:06:10 +0300

    Disable page/route hooks

--- web/src/util/react-transition-component/hooks/usePageTransition.ts
+++ web/src/util/react-transition-component/hooks/usePageTransition.ts
@@ -1,48 +1,48 @@
-'use client';
-
-import type { GuardFunction, SetupTransitionOptions } from '../../core-transition-component';
-import { useTransitionController } from './useTransitionController';
-import { useRouteEnterTransition } from './useRouteEnterTransition';
-
-
-/**
- * Creates gsap.core.Timeline that will start as soon the component is mounted
- *
- * @example
- * const elementRef = useRef<HTMLDivElement>(null);
- *
- * usePageTransition({
- *  beforeTransitionOut: (release) => {
- *    release();
- *  },
- *  beforeTransitionIn: (release) => {
- *    release();
- *  },
- *  crossFlow: true,
- *  setupOptions: () => ({
- *    ref: elementRef,
- *    refs: {
- *      elementRef,
- *    },
- *    setupTransitionInTimeline,
- *    setupTransitionOutTimeline,
- *  }),
- * });
- */
-export function usePageTransition<T>(options: {
-  setupOptions: () => SetupTransitionOptions<T>;
-  beforeTransitionIn?: GuardFunction;
-  // beforeTransitionOut?: GuardFunction;
-  // crossFlow: () => boolean;
-  onDispose?: () => void;
-}): void {
-  const transitionController = useTransitionController(options.setupOptions);
-
-  useRouteEnterTransition(
-    transitionController,
-    {
-      beforeTransitionIn: options.beforeTransitionIn,
-    },
-    options.onDispose,
-  );
-}
+// 'use client';
+//
+// import type { GuardFunction, SetupTransitionOptions } from '../../core-transition-component';
+// import { useTransitionController } from './useTransitionController';
+// import { useRouteEnterTransition } from './useRouteEnterTransition';
+//
+//
+// /**
+//  * Creates gsap.core.Timeline that will start as soon the component is mounted
+//  *
+//  * @example
+//  * const elementRef = useRef<HTMLDivElement>(null);
+//  *
+//  * usePageTransition({
+//  *  beforeTransitionOut: (release) => {
+//  *    release();
+//  *  },
+//  *  beforeTransitionIn: (release) => {
+//  *    release();
+//  *  },
+//  *  crossFlow: true,
+//  *  setupOptions: () => ({
+//  *    ref: elementRef,
+//  *    refs: {
+//  *      elementRef,
+//  *    },
+//  *    setupTransitionInTimeline,
+//  *    setupTransitionOutTimeline,
+//  *  }),
+//  * });
+//  */
+// export function usePageTransition<T>(options: {
+//   setupOptions: () => SetupTransitionOptions<T>;
+//   beforeTransitionIn?: GuardFunction;
+//   // beforeTransitionOut?: GuardFunction;
+//   // crossFlow: () => boolean;
+//   onDispose?: () => void;
+// }): void {
+//   const transitionController = useTransitionController(options.setupOptions);
+//
+//   useRouteEnterTransition(
+//     transitionController,
+//     {
+//       beforeTransitionIn: options.beforeTransitionIn,
+//     },
+//     options.onDispose,
+//   );
+// }
