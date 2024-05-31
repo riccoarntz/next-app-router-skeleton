0f5e924dc7805acfc3249cada3ba43a3c9708500
Ricco Arntz <riccoarntz@gmail.com>
2024-05-31 18:16:04 +0300

    add pre-comit

--- web/lint-staged.config.js
+++ web/lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'web/**/*.{ts?(x),js?(x)}': () => ['npm run lint'],
-  'web/**/*.ts?(x)': () => ['npm run typecheck'],
-  'web/**/*.{ts?(x),js,?(s)css,md,json}': () => ['npm run format'],
+  '/web/**/*.{ts?(x),js?(x)}': () => ['npm run lint'],
+  '/web/**/*.ts?(x)': () => ['npm run typecheck'],
+  '/web/**/*.{ts?(x),js,?(s)css,md,json}': () => ['npm run format'],
 };
