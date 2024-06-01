8148d0af3886eb13e70fb8e7e38d31dcafb680ee
Ricco Arntz <riccoarntz@gmail.com>
2024-06-01 21:04:43 +0300

    add robots.ts example.

---
+++ web/src/app/robots.ts
@@ -0,0 +1,13 @@
+// // import getSettingsData from '@/server/getSettingsData'
+//
+// export default async function robots () {
+//   // const settings = await getSettingsData()
+//   return {
+//     rules: {
+//       userAgent: '*',
+//       allow: '/',
+//       disallow: '/api/*'
+//     },
+//     // sitemap: `${settings.siteUrl}/sitemap.xml`
+//   }
+// }
